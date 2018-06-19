fdad715e0bcd7f0893e8876844054f5faf7a8048
gheinley23 <gheinley23@jacksonindians.us>
2018-06-19 15:05:54 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -1,4 +1,4 @@
-def greeting (local_greeting,your_name,my_name,time_of_day)
+def greeting (local_greeting="Hello",your_name,my_name,time_of_day)
 puts "#{local_greeting} #{your_name}, I am #{my_name} and today is #{time_of_day}"
 end
 
